5f860268b04c9ec710f52505ae7d3032fc0b2487
imahnama <muannama25@gmail.com>
2020-08-12 15:08:31 +0300

    create a navbar

---
+++ src/navbar.js
@@ -0,0 +1,25 @@
+import allProjects from './allProjects'
+
+const navBar = () => {
+  const navDiv = document.createElement('div')
+  navDiv.setAttribute('class', 'navigation border d-flex p-3')
+
+  const navButton = document.createElement('button')
+  navDiv.appendChild(navButton)
+  navButton.setAttribute('class', 'navigation-button btn btn-info ml-auto mr-4')
+
+  const navI = document.createElement('i')
+  navI.setAttribute('class', 'fas fa-plus')
+
+  navButton.addEventListener('click', () => {
+    const createProject = allProjects()
+    createProject.addProject()
+  })
+
+
+  navButton.appendChild(navI)
+
+  document.querySelector('.all-content').appendChild(navDiv)
+}
+
+export default navBar

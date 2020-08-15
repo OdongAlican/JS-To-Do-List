54308c3b9c4a46ea24dd3d6889034091684a5cab
imahnama <muannama25@gmail.com>
2020-08-15 13:15:51 +0300

    add all projects button

--- src/navbar.js
+++ src/navbar.js
@@ -4,6 +4,11 @@ const navBar = () => {
   const navDiv = document.createElement('div');
   navDiv.setAttribute('class', 'navigation border d-flex p-3');
 
+  const viewProjectButton = document.createElement('button');
+  viewProjectButton.innerHTML = 'All Projects'
+  navDiv.appendChild(viewProjectButton);
+  viewProjectButton.setAttribute('class', 'navigation-button btn btn-info');
+
   const navButton = document.createElement('button');
   navDiv.appendChild(navButton);
   navButton.setAttribute('class', 'navigation-button btn btn-info ml-auto mr-4');
@@ -11,9 +16,14 @@ const navBar = () => {
   const navI = document.createElement('i');
   navI.setAttribute('class', 'fas fa-plus');
 
+
   navButton.addEventListener('click', () => {
     const createProject = allProjects();
     createProject.addProject();
+  });
+
+  viewProjectButton.addEventListener('click', () => {
+    window.location.reload()
   });
 
 
